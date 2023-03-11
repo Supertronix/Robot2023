5a556384c65df898df91867b33abce81f223111f
nadineducegep <nadineducegep@gmail.com>
2023-03-11 15:52:36 -0500

    follow pour que les moteurs tournent ensembles

--- src/main/java/frc/robot/soussysteme/Bras.java
+++ src/main/java/frc/robot/soussysteme/Bras.java
@@ -1,10 +1,13 @@
 package frc.robot.soussysteme;
 
+// https://codedocs.revrobotics.com/java/com/revrobotics/cansparkmax
 import com.revrobotics.CANSparkMax;
+// https://codedocs.revrobotics.com/java/com/revrobotics/cansparkmaxlowlevel
+// https://codedocs.revrobotics.com/java/com/revrobotics/cansparkmaxlowlevel.motortype
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
 import frc.robot.Materiel;
 
+// https://codedocs.revrobotics.com/java/com/revrobotics/package-summary.html
 public class Bras implements Materiel.Bras
 {
     protected CANSparkMax moteurPrincipal;
@@ -14,10 +17,18 @@ public class Bras implements Materiel.Bras
     public Bras()
     {
         System.out.println("new Bras()");
-        moteurPrincipal = new CANSparkMax(MOTEUR_PRINCIPAL, MotorType.kBrushless);
-        moteurSecondaire1 = new CANSparkMax(MOTEUR_SECONDAIRE_1, MotorType.kBrushless);
-        moteurSecondaire2 = new CANSparkMax(MOTEUR_SECONDAIRE_2, MotorType.kBrushless);        
+        this.moteurPrincipal = new CANSparkMax(MOTEUR_PRINCIPAL, MotorType.kBrushless);
+        this.moteurSecondaire1 = new CANSparkMax(MOTEUR_SECONDAIRE_1, MotorType.kBrushless);
+        this.moteurSecondaire2 = new CANSparkMax(MOTEUR_SECONDAIRE_2, MotorType.kBrushless);       
+        
+        // REVLibError	follow​(CANSparkMax leader)	
+        // REVLibError	follow​(CANSparkMax.ExternalFollower leader, int deviceID)	
+        // REVLibError	follow​(CANSparkMax.ExternalFollower leader, int deviceID, boolean invert)	
+        // REVLibError	follow​(CANSparkMax leader, boolean invert)	
+        this.moteurSecondaire1.follow(moteurPrincipal);
+        this.moteurSecondaire2.follow(moteurPrincipal);
+        
     }
-    
+
     
 }
