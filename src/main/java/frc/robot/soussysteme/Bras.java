11e58458d38ba18b84e720378123c2a72a105072
nadineducegep <nadineducegep@gmail.com>
2023-03-11 15:32:26 -0500

    Le Bras a trois moteur

--- src/main/java/frc/robot/soussysteme/Bras.java
+++ src/main/java/frc/robot/soussysteme/Bras.java
@@ -8,11 +8,16 @@ import frc.robot.Materiel;
 public class Bras implements Materiel.Bras
 {
     protected CANSparkMax moteurPrincipal;
+    protected CANSparkMax moteurSecondaire1;
+    protected CANSparkMax moteurSecondaire2;
 
     public Bras()
     {
         System.out.println("new Bras()");
         moteurPrincipal = new CANSparkMax(MOTEUR_PRINCIPAL, MotorType.kBrushless);
+        moteurSecondaire1 = new CANSparkMax(MOTEUR_SECONDAIRE_1, MotorType.kBrushless);
+        moteurSecondaire2 = new CANSparkMax(MOTEUR_SECONDAIRE_2, MotorType.kBrushless);        
     }
+    
     
 }
