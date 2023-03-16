affaae6105e9fce0cf284078ed257b08339bedc4
nadineducegep <nadineducegep@gmail.com>
2023-03-16 17:07:11 -0400

    Nouveau mapping de roues

--- src/main/java/frc/robot/Materiel.java
+++ src/main/java/frc/robot/Materiel.java
@@ -5,9 +5,9 @@ public interface Materiel {
     public interface Roues
     {
         public int ROUE_AVANT_DROITE = 4; // ID 4 
-        public int ROUE_ARRIERE_DROITE = 2; // ID 2 
-        public int ROUE_AVANT_GAUCHE = 3; // ID 3 
-        public int ROUE_ARRIERE_GAUCHE = 1; // ID 1     
+        public int ROUE_ARRIERE_DROITE = 3; // ID 2 
+        public int ROUE_AVANT_GAUCHE = 1; // ID 3 
+        public int ROUE_ARRIERE_GAUCHE = 2; // ID 1     
     }
     public interface Manette
     {
