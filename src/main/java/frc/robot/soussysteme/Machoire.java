38812b8a2f71d5fb65fa12368fcd484fe3886fd6
nadineducegep <nadineducegep@gmail.com>
2023-03-26 11:25:50 -0400

    Sous-systeme Machoire

---
+++ src/main/java/frc/robot/soussysteme/Machoire.java
@@ -0,0 +1,59 @@
+package frc.robot.soussysteme;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+// import com.revrobotics.SparkMaxPIDController;
+// https://codedocs.revrobotics.com/java/com/revrobotics/cansparkmaxlowlevel
+import com.revrobotics.SparkMaxRelativeEncoder;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.RelativeEncoder;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import frc.robot.Materiel;
+import frc.robot.composant.Moteur;
+
+@SuppressWarnings("resource") // framework roborio appelle exit
+public class Machoire extends SousSysteme implements Materiel.Machoire
+{
+    protected Moteur moteur;
+
+    public Machoire()
+    {
+        System.out.println("new Machoire()");
+        
+        // creer
+        this.moteur = new Moteur(MOTEUR).avecLimites();
+
+        // configuration
+        this.moteur.setOpenLoopRampRate(0);
+    }
+    public void aller(double position)
+    {
+        System.out.println("Bras.aller()" + position);
+
+    }
+
+    public void fermer() {
+
+    }
+    public void ouvrir() {
+
+    }
+    public void desactiver()
+    {
+        this.moteur.close();
+    }
+    public void initialiser()
+    {
+    }
+    public boolean estAuDepart()
+    {
+        return this.moteur.getLimiteArriere().isPressed();
+    }
+    @Override
+    public void liberer()
+    {
+        this.moteur.liberer();
+    }
+}
