b7518dd8c73317dd9fe0369d9efa96e987a3a34d
nadineducegep <nadineducegep@gmail.com>
2023-03-30 17:35:56 -0400

    Nouveau lecteur de mode autonome - switch brisees

--- src/main/java/frc/robot/interaction/SelecteurPositionAutonome.java
+++ src/main/java/frc/robot/interaction/SelecteurPositionAutonome.java
@@ -1,21 +1,14 @@
 package frc.robot.interaction;
 
 import frc.robot.Materiel;
-
-import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class SelecteurPositionAutonome implements Materiel{
-
-    protected DigitalInput interrupteurGauche = null;
-    protected DigitalInput interrupteurDroit = null;
 
     protected int position = 0;
     
     private SelecteurPositionAutonome()
     {
-        this.interrupteurGauche = new DigitalInput(INTERRUPTEUR_GAUCHE);
-        this.interrupteurDroit = new DigitalInput(INTERRUPTEUR_DROIT);
     }
     
     static protected SelecteurPositionAutonome instance = null;
@@ -27,18 +20,10 @@ public class SelecteurPositionAutonome implements Materiel{
     
     public int lireChoix()
     {
-    	System.out.println("Interrupteur GAUCHE " + this.interrupteurGauche.get());
-    	System.out.println("Interrupteur DROIT " + this.interrupteurDroit.get());
-    	SmartDashboard.putBoolean("Switch GAUCHE", this.interrupteurGauche.get());
-    	SmartDashboard.putBoolean("Switch DROITE", this.interrupteurDroit.get());
-    	
-    	if(this.interrupteurGauche.get() && this.interrupteurDroit.get()) position = 3;
-    	else if(!this.interrupteurGauche.get() && !this.interrupteurDroit.get()) position = 0;
-    	else if(this.interrupteurGauche.get()) position = 2;
-    	else if(this.interrupteurDroit.get()) position = 1;
-    	
-    	System.out.println("Position trouvee " + position);
-    	SmartDashboard.putNumber("Position depart autonome", position);
-		return position;
+        boolean modePlateforme=SmartDashboard.getBoolean("Mode autonome avec plateforme ?", false);
+        if(modePlateforme) position = 3;
+        else position = 0;
+
+        return position;
     }	
 }
